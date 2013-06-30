ce5fea0e5bbc37a5fb66f1b6c92391b19c21f29c
Jacob Lowe <jacoblowe2.0@gmail.com>
2013-06-30 12:24:22 -0700

    adding the ability to set cookies

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ exports.get = function( name ){
 		// since we are using express's default cookies
 		// it has a j: prefix
 		obj = JSON.parse(value.replace(/^j:/, ""));
-	} catch(){
+	} catch( err ){
 		obj = value; 
 		// reset value to string value if error occurs in json parse
 	}
@@ -48,10 +48,14 @@ exports.get = function( name ){
 
 };
 
-// set the cookie is the a way to set a cookie the frist param
+// set the cookie is the a way to set a cookie the first param
 // is the cookie name and the soecond is the payload to be set
 // for the cookies data can be a string or an object
 exports.set = function( name, value, opts ){
+
+	if( !(typeof opts === "object") ){
+		opts = {};
+	}
 
 	if(typeof document === "object"){
 		// literally taken from 
@@ -59,21 +63,24 @@ exports.set = function( name, value, opts ){
 		// so we make the same looking cookies
 		if ('number' == typeof value) value = value.toString();
   		if ('object' == typeof value) value = 'j:' + JSON.stringify(value);
-  		if (null == options.path) options.path = '/';
-  		if ('maxAge' in options) {
-		    options.expires = new Date(Date.now() + options.maxAge);
-		    options.maxAge /= 1000;
+  		if (null == opts.path) opts.path = '/';
+  		if ('maxAge' in opts) {
+		    opts.expires = new Date(Date.now() + opts.maxAge);
+		    opts.maxAge /= 1000;
 		}
 		var cookie = Cookies.serialize( name, value, opts );
-		document.cookie += cookies;
+		document.cookie = cookie;
 		// do client stuff
 	}else if(
 		this.app &&
-		typeof this.app.req === "object"
+		typeof this.app.req === "object" &&
 		typeof this.app.req.res === "object"
 	){
+		var res = this.app.req.res; 
 		// use express to set cookie
-		this.app.req.res.cookie( name, value, opts );
+		if ( !res.headerSent ){
+			res.cookie( name, value, opts );
+		}
 	}
 };
 
